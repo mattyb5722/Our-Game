ecd6f8dba5c0d34a418710c358e9e10f554f72db
Matthew Brown <brownm6115@gmail.com>
2024-10-09 18:16:24 -0400

    Skeleton

---
+++ Assets/Enemy/Skeleton/Skeleton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject projectile;
+    public float projectileSpeed = 10.0f;
+    public int attackSpeed = 2;
+    private int counter = 0;
+
+
+    void FixedUpdate()
+    {
+        counter++;
+
+        if (counter == attackSpeed)
+        {
+            GameObject newProjectile = GameObject.Instantiate(projectile);
+            newProjectile.transform.position = transform.position;
+            newProjectile.SetActive(true);
+            Rigidbody2D r = newProjectile.GetComponent<Rigidbody2D>();
+            r.velocity = (player.transform.position - transform.position).normalized * projectileSpeed;
+
+            counter = 0;
+        }
+    }
+}
