e44540ddeaf9b9e720d39ce0140db70b65236759
BrooksOWallace <brooksow092@gmail.com>
2024-10-09 19:26:12 -0400

    Added Preliminary Rock Grinder Weapon

---
+++ Assets/ParticleCollide.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollide : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other)
+    {
+        print("Shot you");
+    }
+}
