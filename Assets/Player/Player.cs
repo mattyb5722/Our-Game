fa8b3b0b2c73b4fcf8a70aa49ce67419c5161f5b
BrooksOWallace <brooksow092@gmail.com>
2024-10-09 15:52:59 -0400

    Changed Player Controller
    Swapped Controller for Rigid Body + Collider

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
-    public float speedMult = 1.0f;
-    private CharacterController controller;
 
+    public float speedMult = 0.01f;
     public List<Component> weapons = new List<Component>();
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector2 position = transform.position;
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+        position.x = position.x + (speedMult * HorizontalInput);
+        position.y = position.y + (speedMult * VerticalInput);
+        transform.position = position;
+
     }
 
     private void FixedUpdate()
     {
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        controller.Move(movement * 0.2f * speedMult);
+
+        
+
+        Quaternion rotation = transform.rotation;
+
+        Vector3 MousePosition = Input.mousePosition;
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject.name);
     }
 }
